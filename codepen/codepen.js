d7d8e30047e6f2c98ff3d2b722c853c8cb3c50f0
Andreas Larsen <andreas@larsenwork.com>
2015-12-29 15:52:53 +0100

    codepen updates

--- codepen/codepen.js
+++ codepen/codepen.js
@@ -1,9 +1,13 @@
-/*
-https://github.com/larsenwork/social.svg.min
-*/
-
 function solidToggle(event) {
   event.preventDefault()
   var body = document.getElementsByTagName('body')[0]
   body.classList.toggle('no-bg')
+}
+
+function openSource(event) {
+  event.preventDefault()
+  window.open(
+  'https://github.com/larsenwork/social.svg.min',
+  '_blank'
+  )
 }
